bfd9fdef200d0ab23924a92e892698418b4e5a3b
Rui <464362353@qq.com>
2017-07-29 00:31:51 +0800

    优化编辑区

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -15,12 +15,11 @@
       </ol>
     </nav>
     <ol class="panes">
-      <li v-bind:class="{active: currentTab === 0}">tab 1</li>
-      <li v-bind:class="{active: currentTab === 1}">tab 2</li>
-      <li v-bind:class="{active: currentTab === 2}">tab 3</li>
-      <li v-bind:class="{active: currentTab === 3}">tab 4</li>
-      <li v-bind:class="{active: currentTab === 4}">tab 5</li>
-      <li v-bind:class="{active: currentTab === 5}">tab 6</li>
+      <li v-for = 'i in [0,1,2,3,4,5]'
+        v-bind:class="{active: currentTab === i}"
+      >
+        Tab {{ i+1 }}
+      </li>
     </ol>
   </div>
 </template>
